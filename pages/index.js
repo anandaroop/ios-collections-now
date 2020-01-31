7458baad4953ea319acda9a839ca3a22010b1331
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2020-01-31 10:29:58 -0500

    Improve UX

--- pages/index.js
+++ pages/index.js
@@ -9,17 +9,45 @@ const {
 const categoryNames = categories.map(c => c.name)
 
 const collectionsForCategory = categoryName =>
-  categories.filter(cat => cat.name === categoryName)[0].collections
+  categories
+    .filter(cat => cat.name === categoryName)[0]
+    .collections.sort((a, b) => {
+      if (a.title > b.title) return 1
+      if (a.title < b.title) return -1
+      return 0
+    })
 
 function CollectionsIndex() {
   const [query, setQuery] = useState('')
 
   return (
     <main>
+      <style global jsx>{`
+        body {
+          padding: 0;
+          margin: 0;
+        }
+      `}</style>
+
+      <style jsx>{`
+        main {
+          margin: 1em;
+        }
+
+        li {
+          padding: 0.25em 0;
+        }
+
+        input[type='text'] {
+          margin: 0;
+          padding: 0.25em;
+          font-size: 1em;
+          width: calc(100% - 0.5em);
+        }
+      `}</style>
       <form>
         <input
           type="text"
-          size="40"
           placeholder="filter by collection name"
           value={query}
           onChange={e => setQuery(e.target.value)}
